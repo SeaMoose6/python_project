703a9142dd3e712682e473a70de13a5a9d50c9dd
SeaMoose6 <tophtoph05@gmail.com>
2021-09-13 09:40:18 -0400

    started hw

--- python_review.py
+++ python_review.py
@@ -1,3 +1,26 @@
-print("hello world!")
-print("hey!")
-print('i was here')
+'''
+class Rocket():
+    def __init__(self): #defult values second always
+        self.height = 200
+        self.x_loc = 0
+        self.y_loc = 0
+        self.rocket_velo = 10
+
+    def move_up(self):
+        self.y_loc -= self.rocket_velo
+
+
+
+enemy_rockets = [Rocket() for i in range(1, 10)]
+
+
+for rocket in enemy_rockets:
+    print(rocket)
+'''
+
+class Rocket():
+    def __init__(self, x_loc=0, y_loc=0):
+        self.x_loc = x_loc
+        self.y_loc = y_loc
+    def mon_rocket(self, up_dis, down_dis, left_dis, right_dis):
+
